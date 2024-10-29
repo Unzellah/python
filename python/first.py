02416b9cc4be656f323cbfb4c61685297e8583e3
Unzellah <unzellahmsabir@gmail.com>
2024-10-29 14:46:05 +0500

    Add files via upload

---
+++ python/first.py
@@ -0,0 +1,27 @@
+num1 = 20
+num2 = 40
+num3 = 60
+
+# Calculate the sum
+sum = num1 + num2 + num3
+
+# Calculate the average
+average = sum / 3
+
+# Calculate the product
+product = num1 * num2 * num3
+
+# Calculate the differences
+difference12 = num1 - num2
+difference13 = num1 - num3
+difference23 = num2 - num3
+
+# Print the results
+print("Sum: ", sum)
+print("Average: ", average)
+print("Product: ", product)
+print("Difference between first and second: ", difference12)
+print("Difference between first and third: ", difference13)
+print("Difference between second and third: ", difference23)
+
+
---
+++ python/second.py
@@ -0,0 +1,17 @@
+base = 40
+height = 5
+area = 0.5 * base * height
+
+side = 4
+area = side * side
+
+radius = 6
+area = 3.14 * radius * radius
+
+length = 6
+width = 8
+area = length * width
+
+print("TRIANGLE AREA:",area,"SQUARE AREA:", area,"CIRCLE AREA:", area,"RECTANGLE AREA:", area)
+
+
