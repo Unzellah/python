0da58203e814d43d224a1c117bbdcf314e2e63b8
Unzellah Sabir <unzellahmsabir@gmail.com>
2024-11-20 20:12:52 +0500

    Add files via upload
    custom multiplication table printer using for loop and while loop

---
+++ fourth.py
@@ -0,0 +1,4 @@
+i=1
+v1=int(input("Enter Which Number's Table Do You Want?"))
+while 1<=101:
+    
---
+++ third.py
@@ -0,0 +1,4 @@
+v1=int(input("Enter Which Number's Table Do You Want?"))
+for i in range(1,101):
+    print(v1,"x",i,"=",v1*i)
+    i+=1
